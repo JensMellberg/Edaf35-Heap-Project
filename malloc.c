31070a56084b3cf4fbf0ad52f7b1bd6f69ec52eb
JensMellberg <dat14jme@student.lu.se>
2018-05-16 14:11:31 +0200

    Jobbat vidare på linked list

--- malloc.c
+++ malloc.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
 typedef struct block {
 	size_t		      size;
 	struct block   *next;
@@ -15,7 +16,9 @@ typedef struct block {
 
 static block_t *head = NULL;
 
+
 void add(block_t* block) {
+	printf("adding %i to list\n", block);
   if (!head) {
     head = block;
   } else {
@@ -23,9 +26,14 @@ void add(block_t* block) {
     while (current->next && current < block) {
       current = current->next;
     }
-    if (current->next) {
+    if (current >= block) {
+			if (current != head) {
       current->prev->next = block;
-      block->prev = current->prev;
+			block->prev = current->prev;
+			printf("%i next is now %i\n", current->prev, block );
+		  } else {
+				head = block;
+			}
       block->next = current;
       current->prev = block;
     } else {
@@ -34,13 +42,15 @@ void add(block_t* block) {
     }
 
   }
+	print_free();
 }
 
 void remove_(block_t* block) {
-
+	printf("removing block %i, it has prev %i\n", block, block->prev);
   if (block->prev == NULL) {
     if (block->next) {
 			head = block->next;
+			head->prev = NULL;
 		} else {
 			head = NULL;
 		}
@@ -52,31 +62,55 @@ void remove_(block_t* block) {
 		block->next->prev = block->prev;
   }
 
+	print_free();
+
+}
+
+void print_free() {
+	block_t* current = head;
+	printf("Current list: ");
+	while (current) {
+		printf("%i (%i) -> ",current, current->size);
+		current = current->next;
+	}
+	printf("\n" );
 }
 
 void *split(block_t* block, size_t size) {
-
-  block_t *newblock = (block_t *) (block + size + sizeof(block_t));
+	if (size + sizeof(block_t) > block->size)
+		return NULL;
+  block_t *newblock = (block_t *) (((unsigned long)block) + size + sizeof(block_t));
   newblock->size = block->size - (size + sizeof(block_t));
-  block->size = size + sizeof(block_t);
+  block->size = size;
+	block->prev = NULL;
+	block->next = NULL;
+	printf("split created new block at: %i, with size = %i\n",newblock, newblock->size);
+	printf("old block %i,  now has size %i", block, block->size);
 
   return newblock;
 }
 
+
 void *malloc(size_t size) {
+	//printf("malloc call of size %i\n", size);
   if (size == 0) {
     return NULL;
   }
   block_t* current = head;
   while (current) {
-    if (current->size >= size + sizeof(block_t)) {
+    if (current->size >= size) {
+			//printf("found free block %i, with size %i\n",current, current->size);
       remove_(current);
-      if (current->size == size + sizeof(block_t)) {
+      if (current->size == size) {
         return BLOCK_MEM(current);
       }
       //Splitta
       block_t* newblock = split(current, size);
-      add(newblock);
+			newblock->prev = NULL;
+			newblock->next = NULL;
+			if (newblock) {
+        add(newblock);
+		  }
       return BLOCK_MEM(current);
     } else {
       current = current->next;
@@ -85,25 +119,39 @@ void *malloc(size_t size) {
 //Didn't find any free blocks
 block_t* newblock = sbrk(size + sizeof(block_t));
 newblock->size = size;
+newblock->prev = NULL;
+newblock->next = NULL;
+//printf("Malloc created new block at: %i, with size = %i\n",newblock, size);
 return BLOCK_MEM(newblock);
 }
 
 void free(void *ptr) {
+	//printf("free call on memory %i, (block %i)\n", ptr, BLOCK_HEADER(ptr) );
+	if (!ptr) {
+		return NULL;
+	}
   block_t* block = BLOCK_HEADER(ptr);
   add(block);
+	printf("size of freed = %i\n",block->size);
   //Free memory if end of program
   if (block + block->size + sizeof(block_t) == sbrk(0)) {
     sbrk(-(block->size + sizeof(block_t)));
   }
-  //Merge if previous block is free
-  if (block + block->size + sizeof(block_t) == block->next) {
+	//Merge if next block is free
+  if (((unsigned long)block) + block->size + sizeof(block_t) == block->next) {
     block->size = block->size + block->next->size + sizeof(block_t);
     block->next = block->next->next;
+		if (block->next) {
+			block->next->prev = block;
+  	}
+		printf("merging with next block\n");
   }
-  //Merge if next block is free
-  if (block->prev && block - (block->prev->size + sizeof(block_t)) == block->prev) {
+  //Merge if previous block is free
+  if (block->prev && ((unsigned long)block) - (block->prev->size + sizeof(block_t)) == block->prev) {
     block->prev->size = block->prev->size + block->size + sizeof(block_t);
     block->prev->next = block->next;
+		block->next->prev = block->prev;
+		printf("merging with previous block\n");
   }
 }
 
@@ -125,15 +173,18 @@ void *realloc(void *ptr, size_t size) {
   }
   block_t* block = BLOCK_HEADER(ptr);
   if (size <= block->size) {
-    block->size = size;
+
     block_t* newblock = split(block, size);
-    free(newblock);
+		block->size = size;
+		if (newblock) {
+			add(newblock);
+		}
     return ptr;
   } else {
-    block_t* newblock = malloc(size);
-    memcpy(newblock + sizeof(block_t), ptr, block->size);
+    void* newblock = malloc(size);
+    memcpy(newblock, ptr, block->size);
     free(ptr);
-    return BLOCK_MEM(newblock);
+    return newblock;
   }
   return NULL;
 
